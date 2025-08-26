2c01082f6a4a727f8fb541e8c003ef999890671a
Electa-dev <johnwealthtoniloba@gmail.com>
2025-08-26 15:49:39 +0100

    Create banker-roulette.py

---
+++ Sub-projects/banker-roulette.py
@@ -0,0 +1,7 @@
+import random
+
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+roulette_loser = random.choice(friends)
+print(roulette_loser)
+print(f"{roulette_loser} you are to pay the bills.")
+
