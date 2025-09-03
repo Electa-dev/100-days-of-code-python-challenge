f988fccecd34c108c359af6bc09e5d2c6fde01d1
Electa-dev <johnwealthtoniloba@gmail.com>
2025-09-03 16:37:25 +0100

    Create reeborg-hurdle3.py

---
+++ Sub-projects/reeborg-hurdle3.py
@@ -0,0 +1,33 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+def jump2():    
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+while not at_goal():
+    if wall_in_front() != True:
+        move()
+    else:  jump2()
+    
+
+
+
+
