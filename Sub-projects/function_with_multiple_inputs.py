6fd4a5cfe4303c7b1392dd611aeaba04d8b5ae4a
Electa-dev <johnwealthtoniloba@gmail.com>
2025-09-15 12:45:51 +0100

    Create function_with_multiple_inputs.py

---
+++ Sub-projects/function_with_multiple_inputs.py
@@ -0,0 +1,12 @@
+#USING KEYWORD ARGUMENTS
+def greet_with(name, location):
+    print(f"Hello {name}!")
+    print(f"What is it like in {location}?")
+greet_with(name="Electa", location="London")
+
+
+#USING POSITIONAL ARGUMENTS
+def greet_with(name, location):
+    print(f"Hello {name}!")
+    print(f"What is it like in {location}?")
+greet_with("Electa", "London")
