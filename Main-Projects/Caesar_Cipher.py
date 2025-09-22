33bb68b78ec02ef393d1ed022e6e526c8bd7824f
Electa-dev <johnwealthtoniloba@gmail.com>
2025-09-22 16:35:00 +0100

    Create Caesar_Cipher.py

---
+++ Main-Projects/Caesar_Cipher.py
@@ -0,0 +1,35 @@
+import art
+print(art.logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+
+    for letter in original_text:
+        if letter in alphabet:
+            if encode_or_decode == "decode":
+                shifted_position =(alphabet.index(letter) - shift_amount) % len(alphabet)
+            else:
+                shifted_position =(alphabet.index(letter) + shift_amount) % len(alphabet)
+            output_text += alphabet[shifted_position]
+        else: output_text += letter
+    print(f"Here is the {encode_or_decode}d result: {output_text}".title())
+
+go_again = False
+while not go_again:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+
+    decision = input("Type 'yes' if you want to go again. Otherwise, type 'no'\n").lower()
+    if decision == "no":
+        print("Goodbye")
+        go_again = True
+
+
+
+
